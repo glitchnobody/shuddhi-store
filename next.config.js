4df1c19cbd1b2b8b713a7675eb590d5a58641cff
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2024-09-16 18:25:13 +0530

    name change + framer motion

--- next.config.js
+++ next.config.js
@@ -29,6 +29,6 @@ const nextConfig = withStoreConfig({
   },
 })
 
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
+console.log("hentai", JSON.stringify(module.exports, null, 2))
 
 module.exports = nextConfig
